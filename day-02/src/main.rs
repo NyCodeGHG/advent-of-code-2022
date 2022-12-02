e231fc1aa1c614f04742072fd06f530566eb3adb
Marie Ramlow <me@nycode.dev>
2022-12-02 15:25:09 +0100

    Add solution for day 2

---
+++ day-02/src/main.rs
@@ -0,0 +1,70 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("day-02/input.txt").unwrap();
+    let total: i32 = input
+        .lines()
+        .map(|line| {
+            let mut splitted = line.split(' ');
+            let first = splitted.next().unwrap();
+            let second = splitted.next().unwrap();
+
+            let first_score = match first.chars().next().unwrap() {
+                'A' => 1, // Rock
+                'B' => 2, // Paper
+                'C' => 3, // Scissors
+                _ => panic!(),
+            };
+            let second_score = match second.chars().next().unwrap() {
+                'X' => 1, // Rock
+                'Y' => 2, // Paper
+                'Z' => 3, // Scissors
+                _ => panic!(),
+            };
+
+            match first_score - second_score {
+                -1 | 2 => second_score + 6,
+                1 | -2 => second_score,
+                0 => second_score + 3,
+                _ => panic!(),
+            }
+        })
+        .sum();
+    println!("{total}");
+
+    let total: i32 = input
+        .lines()
+        .map(|line| {
+            let mut splitted = line.split(' ');
+            let first = splitted.next().unwrap();
+            let second = splitted.next().unwrap();
+
+            let first_score = match first.chars().next().unwrap() {
+                'A' => 1, // Rock
+                'B' => 2, // Paper
+                'C' => 3, // Scissors
+                _ => panic!(),
+            };
+            match second.chars().next().unwrap() {
+                'X' => match first_score {
+                    1 => 3,
+                    2 => 1,
+                    3 => 2,
+                    _ => panic!(),
+                }, // Loose
+                'Y' => first_score + 3, // Draw
+                'Z' => {
+                    let score = match first_score {
+                        1 => 2,
+                        2 => 3,
+                        3 => 1,
+                        _ => panic!(),
+                    };
+                    score + 6
+                } // Win
+                _ => panic!(),
+            }
+        })
+        .sum();
+    println!("{total}");
+}
